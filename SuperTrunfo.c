b52c0c2514a1bf223f7dd66ae1c42cc3fe1f574d
Ismael Ienczak <ismamobb157@outlook.com>
2025-05-22 14:39:26 -0300

    commit inicial

---
+++ SuperTrunfo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+ 
+ int main (){
+// Variáveis refentes à carta 1:
+   char estado1;
+   char codigo1 [4];
+   char nomecidade1 [20];
+   int populacao1;
+   float area1;
+   float pib1;
+   int turistico1;
+
+// Variáveis referentes à carta 2:
+   char estado2;
+   char codigo2 [4];
+   char nomecidade2 [20];
+   int populacao2;
+   float area2;
+   float pib2;
+   int turistico2;
+
+
+
+
+
+
+
+
+
+
+
+   
+
+// 
+
+
+
+
+
+
+
+}
