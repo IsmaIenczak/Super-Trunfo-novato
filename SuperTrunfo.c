6c21812ca2336a024261899fad4a3f8e67bc30c5
Ismael Ienczak <ismamobb157@outlook.com>
2025-05-22 17:09:16 -0300

    Variáveis e entrada das duas cartas

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -19,22 +19,56 @@
    float pib2;
    int turistico2;
 
+// Entrada de dados em tela (carta 1):
+
+  printf ("Digite uma letra, de (A-H) para escolha da carta 1: ");
+  scanf (" %c", &estado1);
+
+  printf ("Digite a letra escolhida anteriormente seguida de um número 01 a 04. Ex:(A01, B03): ");
+  scanf ("%s", &codigo1);
+
+  printf ("digite o nome da cidade: ");
+  scanf ("%s", &nomecidade1);
+
+  printf ("Digite o número da habitantes da cidade: ");
+  scanf ("%d", &populacao1);
+
+  printf ("Digite a área da cidade em KM²: ");
+  scanf ("%f", &area1);
 
 
+  printf ("Digite o produto interno bruto (PIB) da cidade: ");
+  scanf ("%f", &pib1);
+
+  printf ("A cidade tem quantos pontos turísticos? ");
+  scanf ("%d", &turistico1);
 
 
+// Entrada de dados em tela (Carta 2):
 
+  printf ("Digite uma letra, de (A-H) para escolha da carta 2: ");
+  scanf (" %c", &estado2);
 
+  printf ("Digite a letra escolhida anteriormente seguida de um número 01 a 04. Ex:(A01, B03) para escolha do código da segunda cidade: ");
+  scanf ("%s", &codigo2);
 
+  printf ("digite o nome da segunda cidade: ");
+  scanf ("%s", &nomecidade2);
 
+  printf ("Digite o número da habitantes da segunda cidade: ");
+  scanf ("%d", &populacao2);
 
+  printf ("Digite a área da segunda cidade em KM²: ");
+  scanf ("%f", &area2);
 
-   
+  printf ("Digite o produto interno bruto (PIB) da segunda cidade: ");
+  scanf ("%f", &pib2);
 
-// 
+  printf ("A segunda cidade tem quantos pontos turísticos? ");
+  scanf ("%d", &turistico2);
+  
 
-
-
+  return 0;
 
 
 
